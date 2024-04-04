5da6b49262f6b9a91478f614ca83a2673bf8b398
Wander1ngPanda <jack@ukmurphy.com>
2024-04-04 17:38:11 +0100

    Loaded additional data into local Database for use. Followed the Langchain quickstart to query a website using RAG

---
+++ langchain_quickstart.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=OPENAI_API_KEY)
+
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+print(rag_chain.invoke("What is Hugging?"))
+
+
+vectorstore.delete_collection()
--- load_data.py
+++ load_data.py
@@ -23,6 +23,9 @@ cursor = conn.cursor()
 print('Creating table')
 cursor.execute('''
 DROP TABLE IF EXISTS walmart_ecommerce_product_details;
+                ''')
+conn.commit()
+cursor.execute('''
 CREATE TABLE walmart_ecommerce_product_details
 (
     id SERIAL NOT NULL,
@@ -43,13 +46,67 @@ CREATE TABLE walmart_ecommerce_product_details
     embedding TEXT
 );
                ''')
+conn.commit()
 print('Table created')
 
-df = pd.read_csv('walmart-product-with-embeddings-dataset-usa.csv')
+df = pd.read_csv('local_data/walmart-product-with-embeddings-dataset-usa.csv')
 print(df.head())
 print('Loading data into SQL database')
 df.to_sql('walmart_ecommerce_product_details', con=engine, if_exists='append', index=False, chunksize=10000)
 conn.commit()
+
+print('Data loaded successfully')
+
+print('Creating table')
+cursor.execute('''
+DROP TABLE IF EXISTS evolution_dataset;
+                ''')
+conn.commit()
+cursor.execute('''
+CREATE TABLE evolution_dataset
+(
+    Genus_and_Species VARCHAR(255),
+    Time DECIMAL(18, 8),
+    Location VARCHAR(255),
+    Zone VARCHAR(255),
+    Current_Country VARCHAR(255),
+    Habitat VARCHAR(255),
+    Cranial_Capacity DECIMAL(18, 8),
+    Height DECIMAL(18, 8),
+    Incisor_Size VARCHAR(255),
+    Jaw_Shape VARCHAR(255),
+    Torus_Supraorbital VARCHAR(255),
+    Prognathism VARCHAR(255),
+    Foramen_Magnum_Position VARCHAR(255),
+    Canine_Size VARCHAR(255),
+    Canine_Shape VARCHAR(255),
+    Tooth_Enamel VARCHAR(255),
+    Tecno VARCHAR(255),
+    Tecno_type VARCHAR(255),
+    biped VARCHAR(255),
+    Arms VARCHAR(255),
+    Foots VARCHAR(255),
+    Diet VARCHAR(255),
+    Sexual_Dimorphism VARCHAR(255),
+    Hip VARCHAR(255),
+    Vertical_Front VARCHAR(255),
+    Anatomy VARCHAR(255),
+    Migrated VARCHAR(255),
+    Skeleton VARCHAR(255)
+);
+               ''')
+conn.commit()
+print('Table created')
+
+df = pd.read_csv('local_data/Evolution_DataSets.csv')
+print(df.head())
+print(df.info())
+print('Loading data into SQL database')
+df.to_sql('evolution_dataset', con=engine, if_exists='replace', index=False, chunksize=10000)
+conn.commit()
+
+
+
 conn.close()
 cursor.close()
 engine.dispose()
