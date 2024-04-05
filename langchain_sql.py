3cfa2c4ef44a90839ed1061103d30e4507ba3660
Wander1ngPanda <jack@ukmurphy.com>
2024-04-05 14:49:28 +0100

    SQL Quering is now possible

--- langchain_sql.py
+++ langchain_sql.py
@@ -2,8 +2,12 @@ from pprint import pprint
 from langchain_community.utilities import SQLDatabase
 from langchain.chains import create_sql_query_chain
 from langchain_openai import ChatOpenAI
-
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from dotenv import load_dotenv
+from operator import itemgetter
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain.prompts import PromptTemplate
 import os
 
 load_dotenv()
@@ -18,6 +22,26 @@ db = SQLDatabase.from_uri(f"postgresql://{user}:{password}@{host}:{port}/{dbname
 
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=openai_api_key, temperature=0)
-chain = create_sql_query_chain(llm, db)
-response = chain.invoke({"question": "How many species are in the database?"})
+
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """)
+
+execute_sql_query = QuerySQLDataBaseTool(db=db)
+write_query = create_sql_query_chain(llm, db)
+
+answer = answer_prompt | llm | StrOutputParser()
+chain = (
+    RunnablePassthrough.assign(query=write_query).assign(
+        result=itemgetter("query") | execute_sql_query
+    )
+    | answer
+)
+response = chain.invoke({"question": input("What would you like to ask the database? ")})
 print(response)
+
+
